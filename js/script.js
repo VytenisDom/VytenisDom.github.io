2bc8d2b438ab5fc5261e03e38cc8ce39858c762c
VytenisDom <vytenis.domasevicius@vka.lt>
2019-01-09 18:48:59 +0200

    Added a sidebar for mobile users

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-$('document').ready(function(){
-  var i=0;
+$('document').ready(function () {
+  var i = 0;
+  var opened = false;
   var emailIsSent = false;
   // On load figure out what is the max-content height of see-more section, so we could use it later
   document.getElementById("see-more").style.maxHeight = "max-content";
@@ -14,10 +15,10 @@ $('document').ready(function(){
     var navOffset = $('#nav-ul').height();
     return $('html, body').animate({
       scrollTop: $(this.hash).offset().top - navOffset
-    }, 500, function() {
+    }, 500, function () {
       return window.history.pushState(null, null, target);
     });
-  });  
+  });
   // For smooth scrolling with scrollspy(Footer navigation)
   $("#footer-nav-ul li a[href ^= '#']").on('click', function (event) {
     var target = this.hash;
@@ -28,9 +29,9 @@ $('document').ready(function(){
     }, 500, function () {
       return window.history.pushState(null, null, target);
     });
-  });  
+  });
   // Popup on button click
-  $(".button-projects").on('click', function(event){
+  $(".button-projects").on('click', function (event) {
     $("#modal" + this.id).addClass("active-pop");
     $('body').addClass("scroll-block");
     $('#slider' + this.id).resize(); // Due to the way flexslider is designed, if the parents width is unknown (display none'd), every slide is being given 0px of width
@@ -44,14 +45,14 @@ $('document').ready(function(){
     $('body').removeClass("scroll-block");
     $('.modal-pop').removeClass("revealing-pop");
     $('.modal-pop').addClass("removing-pop");
-    setTimeout(function (){
+    setTimeout(function () {
       $('.modal-pop').removeClass("active-pop");
       $('.modal-pop').removeClass("removing-pop");
-     }, 750);
+    }, 750);
   });
   // Popup close on ESC press
   $(document).keyup(function (e) {
-    if(e.keyCode = 27){ // ESC key
+    if (e.keyCode = 27) { // ESC key
       $('.modal-pop').removeClass("revealing-pop");
       $('.modal-pop').addClass("removing-pop");
       setTimeout(function () {
@@ -62,63 +63,78 @@ $('document').ready(function(){
     }
   });
   // "See more" section expansion w/ animations
-  $('.see-more-btn').on('click', function(event){
+  $('.see-more-btn').on('click', function (event) {
     // Setting the max-height value to the max-content value (If you set straight maxheight = "max-content", then transition (animation) doesn't work.)
     document.getElementById("see-more").style.maxHeight = height;
     $('fade').addClass('hide');
     document.getElementById("see-more-text").innerHTML = "Show less";
-    if(i==0){
-      i=1;
-  }
-  else{
-    document.getElementById("see-more").style.maxHeight = "35vh";
-    $('fade').removeClass('hide');
-    document.getElementById("see-more-text").innerHTML = "Show more";
-    i=0;
-  }
+    if (i == 0) {
+      i = 1;
+    } else {
+      document.getElementById("see-more").style.maxHeight = "35vh";
+      $('fade').removeClass('hide');
+      document.getElementById("see-more-text").innerHTML = "Show more";
+      i = 0;
+    }
   });
   // Mimicing a <a> click on landing page's button
-  $('#video-btn').on('click', function(event){
+  $('#video-btn').on('click', function (event) {
     document.getElementById('video-btn-a').click();
   });
   // Revealing an element when in viewport
   window.addEventListener('scroll', function (e) {
     if (isinViewPort($('.reveal'))) { //Passing an element that I want to check
-        $(".reveal").addClass('revealed');
+      $(".reveal").addClass('revealed');
     }
+  });
+  // Mobile navbar logic
+  $(".openNav").click(function (event) {
+    if (!opened) {
+      $('.sideBar').addClass('navToggle');
+      $('#icon').removeClass('fa fa-bars');
+      $('#icon').addClass('fa fa-times');
+      opened = true;
+    } else {
+      $('.sideBar').removeClass('navToggle');
+      $('#icon').removeClass('fa fa-times');
+      $('#icon').addClass('fa fa-bars');
+      opened = false;
+    }
+  });
+  // Closes the sidebar when <a> tag is clicked
+  $("#sideBar-ul li a[href^='#']").click(function (event) {
+    $(".openNav").trigger("click");
   });
   // Data validation 
   $('.contact-btn').on('click', function (event) {
     console.log(emailIsSent);
-    if(!emailIsSent){
+    if (!emailIsSent) {
       var validated = true;
       if (!validateText('name')) {
         $('#err-name').css("display", "block");
         validated = false;
-      }
-      else {
+      } else {
         $('#err-name').css("display", "none");
       }
       if (!validateEmail('email')) {
         $('#err-email').css("display", "block");
         validated = false;
-      }
-      else {
+      } else {
         $('#err-email').css("display", "none");
       }
       if (!validateText('message')) {
         $('#err-msg').css("display", "block");
         validated = false;
-      }
-      else {
+      } else {
         $('#err-msg').css("display", "none");
       }
       if (validated) {
         sendEmail(String(document.getElementById('email').value));
       }
-    }    
+    }
   });
 });
+
 function isinViewPort(element) {
   if (element.length == 0) {
     return; //If the passed element doesn't exist, return.
@@ -133,20 +149,22 @@ function isinViewPort(element) {
   var bottom = top + height;
   return (bottom <= screenBottom);
 }
-function validateText(name){
-  if(document.getElementById(name).value == ""){
+
+function validateText(name) {
+  if (document.getElementById(name).value == "") {
     return false;
-  }
-  else{
+  } else {
     return true;
   }
 }
-function validateEmail(id){
+
+function validateEmail(id) {
   var email = String(document.getElementById(id).value);
   var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email); 
+  return regex.test(email);
 }
-function sendEmail(email){
+
+function sendEmail(email) {
   // Just so it wouldn't be visible to spam bots:D
   var who = "vytenisd";
   var at = '@';
@@ -154,14 +172,12 @@ function sendEmail(email){
   Email.send(who + at + where,
     who + at + where,
     "Contact form on portfolio website, Name : " + document.getElementById('name').value + " , Sender: " + email,
-    document.getElementById('message').value,
-    {
+    document.getElementById('message').value, {
       token: "31e69309-baa6-4455-bc10-0b0f9b42e2c7",
       callback: function done() {
         document.getElementById('send').innerHTML = "Email has been sent";
-        $('#send').css("cursor","default");
+        $('#send').css("cursor", "default");
         emailIsSent = true;
       }
     });
 }
-
